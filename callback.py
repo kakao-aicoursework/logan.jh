c7410659e768bebf201cf7a3cac3d94b763fbf77
logan-jh <logan.jh@kakaocorp.com>
2023-12-12 18:21:25 +0900

    use requests module

--- callback.py
+++ callback.py
@@ -1,8 +1,8 @@
 from dto import ChatbotRequest
-import aiohttp
 import time
 import logging
 import os
+import requests
 
 from langchain import LLMChain
 from langchain.chat_models import ChatOpenAI
@@ -15,7 +15,7 @@ from langchain.schema import (
 )
 
 # 환경 변수 처리 필요!
-os.environ['OPENAI_API_KEY'] = ''
+os.environ['OPENAI_API_KEY'] = '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'
 SYSTEM_MSG = "당신은 카카오 서비스 제공자입니다."
 logger = logging.getLogger("Callback")
 
@@ -25,9 +25,9 @@ def read_from_file(file_path: str) -> str:
 
 kakaotalk_sync_txt = read_from_file('project_data_kakaosync.txt')
 
-async def callback_handler(request: ChatbotRequest) -> dict:
+def callback_handler(request: ChatbotRequest) -> dict:
     system_message = "assistant는 챗봇으로 동작한다. 챗봇은 '제품정보' 내용을 참고하여, user의 질문 혹은 요청에 따라 적절한 답변을 제공합니다."
-    human_template = (request.userRequest.utterance + "\n\n")
+    human_template = (request.userRequest.utterance)
 
     system_message_prompt = SystemMessage(content=system_message)
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
@@ -58,11 +58,8 @@ async def callback_handler(request: ChatbotRequest) -> dict:
     # 참고링크1 : https://kakaobusiness.gitbook.io/main/tool/chatbot/skill_guide/answer_json_format
 
     print(payload)
-    time.sleep(1.0)
 
     url = request.userRequest.callbackUrl
 
     if url:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url=url, json=payload, ssl=False) as resp:
-                await resp.json()
+        return requests.post(url, json=payload).json()
